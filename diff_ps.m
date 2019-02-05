47e5c97ee5c2b263cf6dad9217d2d8a365f1cceb
Paul Alexander <paulalexander23@live.co.uk>
2019-02-05 10:46:00 +0000

    Reduce number of new variables created in the pseudo spectral differation.

--- diff_ps.m
+++ diff_ps.m
@@ -1,4 +1,4 @@
-function dy = diff_ps(y, degree, xL, suppression)
+function out = diff_ps(y, degree, xL, suppression)
     %DIFF_PSEUDO_SPECTRAL Uses the pseudo-spectral method to differentiate
     %   Detailed explanation goes here
     if nargin < 4
@@ -6,25 +6,25 @@ function dy = diff_ps(y, degree, xL, suppression)
     end
 
     % Transform into fourier space
-    yF = fft(y);
+    out = fft(y);
 
-    N = size(yF,1)/2;
+    N = size(out,1)/2;
 
     % Determine k in matlab form
     %k = fftshift([0,-N+1:N-1])';
     k = [0:N-1, 0, 1-N:-1]' * 2*pi/xL;
 
     % Prior suppression
-    yF(abs(yF)<suppression) = 0;
+    out(abs(out)<suppression) = 0;
 
     % Apply pseudo-spectral differentiation
-    dyF = (1i*k).^degree.*yF;
+    out = (1i*k).^degree.*out;
 
     % Posterior suppression
     % dyF(abs(dyF) < suppression*N*2) = 0 ;
     % dyF(abs(dyF) < suppression*max(abs(dyF))) = 0 ;
 
     % Transform back into real space
-    dy = ifft(dyF);
+    out = ifft(out);
 
 end
