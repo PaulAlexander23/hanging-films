9177cfaf41bb472e5d936ce358a4ea7a727aef5a
Paul Alexander <paulalexander23@live.co.uk>
2019-03-28 13:33:10 +0000

    Update main.m.

--- main.m
+++ main.m
@@ -1,57 +1,117 @@
 %MAIN
 
-%% SETUP 
+addpath("../time-stepping-methods/",...
+    "../optimisation-methods/",...
+    "../discretisation-methods/")
 
-xN = 2^7;
-xL = 10;
-xS = xL/xN;
-x = linspace(xS,xL,xN)';
+%% SETUP x
+dim = 2;
+xL = [32,32];
+xN = [24,40];
+xS = xL./xN;
+x = cell(dim,1);
+for n = 1:dim
+    x{n} = linspace(xS(n),xL(n),xN(n))';
+end
 
-yN = 2^7;
-yL = 10;
-yS = yL/yN;
-y = linspace(yS,yL,yN);
+%% SETUP t
 
-V = zeros(1,1,25,2);
-V(1,1,:,:) = combvec(1:5,1:5)';
+tL = 1;
+tN = 10;
+tS = tL/tN;
+t = 0:tS:tL;
 
-R1 = rand(1,1,25);
-R2 = rand(1,1,25);
-R3 = rand(1,1,25);
-R4 = rand(1,1,25);
+%% SETUP y0
+% A = 1e-1;
+% y0 = 1 + A * (-cos(2*pi/xL(1) * x{1}) + 0*x{2}');
+% y0 = 1 + A * (-cos(2*pi/xL(1) * x{1}) + sin(2*pi/xL(2) * x{2}'));
 
-H0 = 1 ...
-    + sum(1e-4 * R1 .* cos(V(1,1,:,1) .* x + V(1,1,:,2) .* y + 2*pi*R2),3)...
-    + sum(1e-4 * R3 .* cos(V(1,1,:,1) .* x - V(1,1,:,2) .* y + 2*pi*R4),3);
+y0 = h0;
 
-% delta, theta, Re, We, C
-params = [1,pi/4,1,0,1];
+% V = zeros(1,1,25,2);
+% V(1,1,:,:) = combvec(1:5,1:5)';
+% R1 = rand(1,1,25);
+% R2 = rand(1,1,25);
+% R3 = rand(1,1,25);
+% R4 = rand(1,1,25);
+% y0 = 1 ...
+%     + sum(1e-4 * R1 .* cos(V(1,1,:,1) .* x{1}' + V(1,1,:,2) .* x{2} + 2*pi*R2),3)...
+%     + sum(1e-4 * R3 .* cos(V(1,1,:,1) .* x{1}' - V(1,1,:,2) .* x{2} + 2*pi*R4),3);
 
-tFinal = 1;
+% plot_surface(x,y0')
 
-%plot_surface(x,y,H0)
+%% SETUP problem
+params = [1,7*pi/8,1.0,0,0.01]; % delta, theta, Re, We, C
+problemDeg = [1,0;0,1;2,0;0,2]';
+
+% SETUP differentiation method
+D = init_fd(x, problemDeg, 2);
+method = @(x,y,deg) diff_fd(x,y,deg,D,problemDeg);
+getD = @(deg) get_fd(deg,D,problemDeg);
+
+%method = @diff_ps_2d;
+
+%problem = @fbenney;
+%problem = @(x,y,params,method) fbenney2(x,y,params,method,getD);
+pdefun = @(t,x,y,method) benney(x,y,params,method,getD);
+
+%% SETUP timestepping method
+
+% options = optimoptions('fsolve',...
+%    'SpecifyObjectiveGradient',true,...
+%    'Display','off');
+% optimmethod = @(fun,x0) fsolve(fun,x0,options);
+optimmethod = @newton;
+timestepper = @(odefun,t,y0) bdf3(odefun,t,y0,optimmethod);
+
+evnt = @(~,H) is_dewetted(H);
+
+RelTol = 1e-3;
+options = odeset(...
+    ...'Vectorized','on',...
+    ...'BDF','on',... % Backward differentiation formulae
+    'Event',@(t,y) event(t,y),...
+    ...'OutputFcn','odeprint',...
+    'OutputFcn',@outputfunction,...
+    'RelTol',RelTol...
+    ); % Default: 1e-3
 
 %% Solve
-
 tic
-[H, ~, t] = solver(@f_benney, params, H0, tFinal, [xL, yL], [xN, yN], @(~,H) is_dewetted(H), 1e-3);
+[y, t] = solver(pdefun, t, x, y0, method, timestepper, options);
 toc
 
 %% Plot overview
 
-plot_surface(x,y,H(:,:,1));
-
-plot_surface(x,y,H(:,:,end));
+plot_surface(x,real(y(:,:,end))');
 
 figure
-plot(t, squeeze(sum((H-1).^2 * xS * yS,[1,2])))
+plot(t, squeeze(sum((y-1).^2 * x{1}(1) * x{2}(1),[1,2])))
 
 %% Plot specific
 
-plot_surface(x,y,H(:,:,90));
+plot_surface(x,real(y(:,:,3))');
+
+%plot_surface(x,real(y(:,:,end)-y0)');
 
 
 %% Save
 
 filename = replace(sprintf('data-%g-%g-%g-%g-%g.mat',params),'.','_');
-save(filename,'H','H0','params','t','x','y');
+save(filename,'x','t','y','params');
+
+%% Functions
+function [value, isterminal, direction] = event(t,y)
+    Y = reshape(y,shape);
+    value = 2*evnt(t,Y) - 1;
+    if any(isnan(y),'all')
+        value = 1;
+    end
+    isterminal = 1; % Terminal
+    direction = 0; % Any approach direction
+end
+
+function [status] = outputfunction(t,y,flags)
+    %whos;
+    status = 0;
+end
