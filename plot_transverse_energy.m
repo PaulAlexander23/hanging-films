2bb6e5326eaef308a981c04d3b30b6ce8b855011
Paul Alexander <paulalexander23@live.co.uk>
2019-04-10 14:01:58 +0100

    Rename project transverse to something sensible.

--- plot_transverse_energy.m
+++ plot_transverse_energy.m
@@ -1,13 +1,14 @@
-function project_transverse(t, x, y)
+function plot_transverse_energy(t, x, y)
     
     y = reshape(y, [cellfun(@length, x)', length(t)]);
     
     fh = fft2(y);
     
+    %fh(:,2:end) = 0;
     fh(2:end,:) = 0;
     
     fh = ifft2(fh);
     
-    plot(t, energy(fh, cellfun(@length, x)))
+    plot(t, energy(x, fh))
     
 end
