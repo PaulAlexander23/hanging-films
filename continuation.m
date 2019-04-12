5ba47de33635416330a45a2859de09d3a7742c2c
Paul Alexander <paulalexander23@live.co.uk>
2019-04-12 14:34:18 +0100

    Add file to collect results from the continuation test runs.

---
+++ continuation.m
@@ -0,0 +1,21 @@
+
+
+files = dir("*.mat");
+number_of_files = length(files);
+
+Re = zeros(number_of_files,1);
+initial_min_h = zeros(number_of_files,1);
+final_min_h = zeros(number_of_files,1);
+
+for file_number = 1:number_of_files
+    load(files(file_number).name);
+    
+    Re(file_number) = params(3);
+    initial_min_h(file_number) = min(y(:,:,1),[],'all');
+    final_min_h(file_number) = min(y(:,:,end),[],'all');
+end
+
+plot([0,1.5],[1,1]);
+hold on
+plot(Re,initial_min_h);
+plot(Re,final_min_h);
