0db1c7e009294aa11148ab76176efe4f1884aece
Paul Alexander <paulalexander23@live.co.uk>
2019-04-15 14:36:30 +0100

    Add option to customise rivulet initial condition.

--- irivulet.m
+++ irivulet.m
@@ -1,7 +1,7 @@
-function y0 = irivulet(x)
+function y0 = irivulet(x, A, r)
     
-    A = 2e-1;
-    r = 0.05;
+    if nargin < 2, A = 2e-1; end
+    if nargin < 3, r = 0.05; end
     y0 = 1 + A * (-r*cos(2*pi/x{1}(end) * x{1}) - cos(2*pi/x{2}(end) * x{2}'));
     
 end
