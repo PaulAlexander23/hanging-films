a1cfc97b0301cdfa046cccfe5cdb34db78940fe7
Paul Alexander <paulalexander23@live.co.uk>
2019-05-01 16:44:11 +0100

    Add function to generate a single mode in both directions with given amplitudes.

---
+++ icos.m
@@ -0,0 +1,8 @@
+function y0 = icos(x, a, b)
+
+    if nargin < 2, a = 0.25; end
+    if nargin < 3, b = 0.25; end
+    y0 = 1 - a * cos(2*pi/x{1}(end) * x{1}) - ...
+        b * cos(2*pi/x{2}(end) * x{2}');
+    
+end
