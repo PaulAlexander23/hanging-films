b37b3fc5db0f981537a1424218b7f304b8addeb7
Paul Alexander <paulalexander23@live.co.uk>
2019-05-01 17:43:43 +0100

    Update irand to include arguments for amplitude and number of modes. Rewrite in terms of ifft2.

--- irand.m
+++ irand.m
@@ -1,15 +1,20 @@
-function y0 = irand(x)
-    %IRAND Interface on x with 5 random modes
+function y0 = irand(x, A, num)
+    %IRAND Interface on x with "num" random modes and amplitude "A".
     
-    A = 0.1;
-    V = zeros(1,1,25,2);
-    V(1,1,:,:) = combvec(1:5,1:5)';
-    R1 = rand(1,1,25);
-    R2 = rand(1,1,25);
-    R3 = rand(1,1,25);
-    R4 = rand(1,1,25);
-    y0 = 1 ...
-        + sum(A * R1 .* cos(V(1,1,:,1) .* x{1} + V(1,1,:,2) .* x{2}' + 2*pi*R2),3)...
-        + sum(A * R3 .* cos(V(1,1,:,1) .* x{1} - V(1,1,:,2) .* x{2}' + 2*pi*R4),3);
+    if nargin < 2, A = 1e-4; end
+    if nargin < 3, num = 5; end
+    Nx = length(x{1})/2;
+    Ny = length(x{2})/2;
+
+    % Determine k in matlab form
+    kx = [0:Nx-1, 0, 1-Nx:-1]';
+    ky = [0:Ny-1, 0, 1-Ny:-1]';
+    
+    fy0 = zeros(2*Nx,2*Ny);
+    
+    fy0(logical((kx~=0).*(abs(kx)<num+1)),logical((ky~=0).*(abs(ky)<num+1))) = (2*rand(2*num,2*num)-1)*Nx*Ny;
+    
+    y0 = real(ifft2(fy0));
+    y0 = 1 + A * y0/max(abs(y0),[],[1,2]);
     
 end
