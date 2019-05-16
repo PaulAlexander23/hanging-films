5ce99c719f65b79c3b603c6c07a72cb6f8fd628a
Paul Alexander <paulalexander23@live.co.uk>
2019-05-16 16:22:36 +0100

    Rescale energy using the lengths of the domain to allow comparision between domains.

--- energy.m
+++ energy.m
@@ -1,8 +1,9 @@
 function E = energy(x, y)
 
     dx = cellfun(@(x) x(1), x);
+    xl = cellfun(@(x) x(end), x);
     
-    E = sum((y - 1).^2*prod(dx),[1,2]);
+    E = sum((y - 1).^2*prod(dx)/prod(xl),[1,2]);
     
     E = squeeze(E);
 end
