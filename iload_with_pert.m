2aa1c60913ddd957e18e7fb6c8726038e8de3567
Paul Alexander <paulalexander23@live.co.uk>
2019-05-30 14:51:49 +0100

    Correct scaling on perturbation.

--- iload_with_pert.m
+++ iload_with_pert.m
@@ -1,5 +1,5 @@
 function y0 = iload_with_pert(x,filename,alpha)
     if nargin < 3, alpha = 1e-2; end
     load(filename,'h');
-    y0 = h + alpha * cos(x{1} + x{2});
+    y0 = h + alpha * cos(2*pi*(x{1}/x{1}(end) + x{2}/x{2}(end)));
 end
