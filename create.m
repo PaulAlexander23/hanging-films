11893cf7309b9a117c710e8fca2d4caaf2283860
Paul Alexander <paulalexander23@live.co.uk>
2019-06-07 09:03:04 +0100

    Split out into more functions.

--- create.m
+++ create.m
@@ -1,45 +1,52 @@
-function create(theta,Re,C,x_length,y_length,t_final,interface,xN,yN,AbsTol)
-    
+function create(theta, Re, C, xLength, yLength, tFinal, interface, xN, yN, AbsTol)
+
     if nargin < 8, xN = 64; end
     if nargin < 9, yN = 64; end
     if nargin < 10, AbsTol = 1e-6; end
-    
-    dim = 2;
-    xL = [x_length,y_length];
-    xN = [xN,yN];
-    xS = xL./xN;
-    x = cell(dim,1);
-    for n = 1:dim
-        x{n} = linspace(xS(n),xL(n),xN(n))';
-    end
-    
-    t = [0,t_final];
-    
+
+    x = setupX(xLength, yLength, xN, yN);
+
+    t = [0, tFinal];
+
     y0 = interface(x);
-    
-    params = [1,theta,Re,C]; % delta, theta, Re, C
-    problemDeg = [1,0;0,1;2,0;0,2]';
-    
-    D = init_fd(x, problemDeg, 4);
-    method = @(x,y,deg) diff_fd(x,y,deg,D,problemDeg);
-    getD = @(deg) get_fd(deg,D,problemDeg);
-    pdefun = @(t,x,y,method) fbenney(x,y,params,method);
-    
+
+    params = [1, theta, Re, C]; % delta, theta, Re, C
+    problemDiffDegrees = [1, 0; 0, 1; 2, 0; 0, 2]';
+
+    diffMat = init_fd(x, problemDiffDegrees, 4);
+    diffMethod = @(x, y, degree) diff_fd(x, y, degree, diffMat, problemDiffDegrees);
+    getDiffMat = @(deg) get_fd(deg, diffMat, problemDiffDegrees);
+    pdeFunction = @(t, x, y, diffMethod) fbenney(x, y, params, diffMethod);
+
     odeopt = odeset( ...
-        'Jacobian', @(t, y) jbenney(x, y, params, method, getD), ...
+        'Jacobian', @(t, y) jbenney(x, y, params, diffMethod, getDiffMat), ...
         'Event', @event_dewetted, ...
         'Vectorized', 'on', ...
         'AbsTol', AbsTol ...
-        ...'BDF','on' ... % Backward differentiation formulae
+        ... 'BDF','on' ...
         );
-    timestepper = @(odefun,t,y0) ode15s(odefun,t,y0,odeopt);
-    
+    timeStepper = @(odefun, t, y0) ode15s(odefun, t, y0, odeopt);
+
     tic
-    [y, t] = solver(pdefun, t, x, y0, method, timestepper);
+    [y, t] = solver(pdeFunction, t, x, y0, diffMethod, timeStepper);
     timeTaken = toc;
-    
+
+    saveData(y, params, t, x, timeTaken, tFinal, interface, AbsTol)
+end
+
+function x = setupX(xLength, yLength, xN, yN)
+    xL = [xLength, yLength];
+    xN = [xN, yN];
+    xS = xL ./ xN;
+    dim = 2;
+    x = cell(dim, 1);
+    for n = 1:dim
+        x{n} = linspace(xS(n), xL(n), xN(n))';
+    end
+end
+
+function saveData(y, params, t, x, timeTaken, tFinal, interface, AbsTol)
     filename = replace(sprintf('data-theta-%g-Re-%g-C-%g-xL-%g-yL-%g-T-%g-interface-%s-xN-%g-yN-%g-AbsTol-%g', ...
-        theta,Re,C,x_length,y_length,t_final,func2str(interface),xN(1),xN(2),AbsTol),'.','_');
-    save(filename,'y','params','t','x','timeTaken');
-    
+        params(2), params(3), params(4), x{1}(end), x{2}(end), tFinal, func2str(interface), length(x{1}), length(x{2}), AbsTol), '.', '_');
+    save(filename, 'y', 'params', 't', 'x', 'timeTaken');
 end
