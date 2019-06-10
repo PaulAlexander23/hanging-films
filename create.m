c01d7dcccdca89d50410fabda1b97296c3a48775
Paul Alexander <paulalexander23@live.co.uk>
2019-06-10 09:15:01 +0100

    Update create.m in light of recent changes.

--- create.m
+++ create.m
@@ -16,19 +16,18 @@ function create(theta, Re, C, xLength, yLength, tFinal, interface, xN, yN, AbsTo
     diffMat = init_fd(x, problemDiffDegrees, 4);
     diffMethod = @(x, y, degree) diff_fd(x, y, degree, diffMat, problemDiffDegrees);
     getDiffMat = @(deg) get_fd(deg, diffMat, problemDiffDegrees);
-    pdeFunction = @(t, x, y, diffMethod) fbenney(x, y, params, diffMethod);
+    pdeFunction = @(t, x, y, diffMethod) fbenney2d(x, y, params, diffMethod);
 
     odeopt = odeset( ...
-        'Jacobian', @(t, y) jbenney(x, y, params, diffMethod, getDiffMat), ...
-        'Event', @event_dewetted, ...
-        'Vectorized', 'on', ...
-        'AbsTol', AbsTol ...
+        'Jacobian', @(t, y) jbenney(x, y, params, diffMethod, getDiffMat) ...
+        ...'Vectorized', 'on', ...
+        ...'AbsTol', AbsTol ...
         ... 'BDF','on' ...
         );
     timeStepper = @(odefun, t, y0) ode15s(odefun, t, y0, odeopt);
 
     tic
-    [y, t] = solver(pdeFunction, t, x, y0, diffMethod, timeStepper);
+    [y, t] = pdeSolver(pdeFunction, t, x, y0, diffMethod, timeStepper);
     timeTaken = toc;
 
     saveData(y, params, t, x, timeTaken, tFinal, interface, AbsTol)
@@ -45,8 +44,3 @@ function x = setupX(xLength, yLength, xN, yN)
     end
 end
 
-function saveData(y, params, t, x, timeTaken, tFinal, interface, AbsTol)
-    filename = replace(sprintf('data-theta-%g-Re-%g-C-%g-xL-%g-yL-%g-T-%g-interface-%s-xN-%g-yN-%g-AbsTol-%g', ...
-        params(2), params(3), params(4), x{1}(end), x{2}(end), tFinal, func2str(interface), length(x{1}), length(x{2}), AbsTol), '.', '_');
-    save(filename, 'y', 'params', 't', 'x', 'timeTaken');
-end
