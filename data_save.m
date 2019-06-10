1793cc4982837f861119bd149d9bf217bf8c4cdb
Paul Alexander <paulalexander23@live.co.uk>
2019-06-10 09:06:51 +0100

    Rename data_save saveData.

--- data_save.m
+++
@@ -1,5 +0,0 @@
-%DATA_SAVE
-
-filename = replace(sprintf('data-theta-%g-Re-%g-C-%g-xL-%g-yL-%g-T-%g-interface-%s-xN-%g-yN-%g-AbsTol-%g', ...
-    theta,Re,C,x_length,y_length,t_final,func2str(interface),xN(1),xN(2),AbsTol),'.','_');
-save(filename,'y','params','t','x','timeTaken');
