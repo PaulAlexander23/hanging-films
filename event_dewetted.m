f4adb7826f60e672b72ace9a620aa8388d4875c8
Paul Alexander <paulalexander23@live.co.uk>
2019-06-10 09:09:29 +0100

    Remove event_dewetted file as we are not expecting dewetting.

--- event_dewetted.m
+++
@@ -1,9 +0,0 @@
-function [value, isterminal, direction] = event_dewetted(~, y)
-   
-    y = reshape(y, numel(y), 1);
-    value = 2 * any(y < 0) - 1; % -1 if wetted, 1 if dewetted
-    if any(isnan(y)), value = 1; end % 1 if Nans in solution
-    isterminal = 1; % Terminates
-    direction = 0; % Allow any approach direction
-    
-end
