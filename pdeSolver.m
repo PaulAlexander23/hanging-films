8cef7c5a73ca12994f294471b6c802f9ba8224fa
Paul Alexander <paulalexander23@live.co.uk>
2019-06-10 09:08:50 +0100

    Rename solver pdeSolver.

---
+++ pdeSolver.m
@@ -0,0 +1,28 @@
+function [y, t] = pdeSolver(pdefun, t, x, y0, diffMethod, timestepper)
+    
+    shape = size(y0);
+    y0 = reshape(y0, [prod(shape), 1]);
+    
+    y = timestepper(@(t, y) callPDEFunctionWithReshape(pdefun, ...
+        t, x, y, diffMethod, shape), t, y0);
+    
+    if isstruct(y)
+        t = y.x;
+        y = y.y;
+    end
+    
+    y = squeeze(reshape(y, [shape, length(t)]));
+end
+
+function F = callPDEFunctionWithReshape(pdefun, t, x, y, diffMethod, shape)
+    f = pdefun(t, x, reshapeInput(y, shape), diffMethod);
+    F = reshapeOutput(y, f, shape);
+end
+
+function out = reshapeInput(y, shape)
+    out = reshape(y, [shape, numel(y) / prod(shape)]);
+end
+
+function F = reshapeOutput(y, f, shape)
+    F = reshape(f, [prod(shape), numel(y) / prod(shape)]);
+end
