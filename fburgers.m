df1e6901b5e0bf03c8a8130341fd6804c572485e
Paul Alexander <paulalexander23@live.co.uk>
2019-06-10 09:11:34 +0100

    Remove extra function files not related to the project.

--- fburgers.m
+++
@@ -1,7 +0,0 @@
-function F = fburgers(x,y,params,method)
-    deg = [0,1;2,0;0,2]';
-    D = params(1);
-    dy = feval(method,x,y,deg);
-    F = 10*dy(:,:,1)... * y.*dy
-        - D*(dy(:,:,2) + dy(:,:,3));
-end
