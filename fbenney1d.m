8ab668ef4eaa5d4b79208ee32d5660c89c5900f6
Paul Alexander <paulalexander23@live.co.uk>
2019-06-10 09:13:10 +0100

    Improve how fbenney1d calls the differentiation function.

--- fbenney1d.m
+++ fbenney1d.m
@@ -1,11 +1,12 @@
 function F = fbenney1d(x,y,params,method)
-    deg = [1,2];
+    
     delta = params(1);
     theta = params(2);
     Re = params(3);
     C = params(4);
     
-    dy = feval(method,x,y,deg);
+    deg = [1,2];
+    dy = method(x,y,deg);
     
     P = y * cot(theta) - 1/2 * 1/C * (dy{2});
     
