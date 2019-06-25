9ce41e93ff90b63108182fd8c4cc6e552c492d9f
Paul Alexander <paulalexander23@live.co.uk>
2019-06-25 16:31:08 +0100

    Update mainRuben with new discretisationMethods.

--- mainRuben.m
+++ mainRuben.m
@@ -1,45 +1,24 @@
-%MAIN
+addpath discretisationMethods
 
-import discretisationMethods.*
 
-%% SETUP x
-dim = 2;
-xL = [32, 32]; % [x length, y length] - Change here to 16 for other one.
-xN = [64, 64];
-xS = xL ./ xN;
-x = cell(dim, 1);
-for n = 1:dim
-    x{n} = linspace(xS(n), xL(n), xN(n))';
-end
-
-%% SETUP h
+x = setupX(32,32,64,64);
+params = [1, 7 * pi / 8, 1, 0.01]; % delta, theta, Re, C
+domain = FDDomain(x, [1, 0; 0, 1; 2, 0; 0, 2]', 4); % Finite Difference Domain
+% domain = PSDomain(x); % Finite Difference Domain
+pdefun = @(t, domain, y) fwibl1(domain, y, params);
 
 % h = load('wibl1DataForRuben.mat','h'); % Note: h is y and F combine
+
 y = h(1:end/2,:);
 F = h(end/2+1:end,:);
 % F = zeros(size(F)); % set F1 to zero instead of using numerics.
 
-% surf(y)
-
-%% SETUP problem
-params = [1, 7 * pi / 8, 1, 0.01]; % delta, theta, Re, C
-
-% SETUP differentiation method
-problemDeg = [1, 0; 0, 1; 2, 0; 0, 2]';
-D = init_fd(x, problemDeg, 4);
-getD = @(deg) get_fd(deg, D, problemDeg);
-diffMethod = @(x, y, deg) diff_fd(x, y, deg, D, problemDeg);
-
-pdefun = @(t, x, y, method) fwibl1(x, y, params, method);
-
-%%
-
-dhdt = fwibl1(x, h, params, diffMethod);
+dhdt = fwibl1(domain, h, params);
 dydt = dhdt(1:end/2,:);
 dFdt = dhdt(end/2+1:end,:);
 
 %%
-dydx = cell2mat(diffMethod(x, y, [1, 0]'));
+dydx = domain.diff(y, [1, 0]');
 
 c = mean(dydt./-dydx,'all');
 
@@ -53,7 +32,7 @@ figure
 surf(- c * dydx)
 
 %%
-dFdx = cell2mat(diffMethod(x, F, [1, 0]'));
+dFdx = domain.diff(F, [1, 0]');
 
 c = mean(dFdt./-dFdx,'all');
 
