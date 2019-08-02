03ce58cee95b0af52edc9092d5de34d190df39d6
Paul Alexander <paulalexander23@live.co.uk>
2019-08-02 15:49:34 +0100

    Rename and format.

--- pdeSolver.m
+++ pdeSolver.m
@@ -2,22 +2,26 @@ function [y, t] = pdeSolver(pdefun, t, domain, y0, timestepper)
     shape = size(y0);
     y0 = reshape(y0, [prod(shape), 1]);
     
-    [t, y] = timestepper(@(t, y) callPDEFunctionWithReshape(pdefun, ...
-        t, domain, y, shape), t, y0);
+    [t, y] = timestepper( ...
+        @(t, y) pdefunVector(pdefun, t, domain, y, shape), ...
+        t, y0);
+    
     y = y.';
     y = squeeze(reshape(y, [shape, length(t)]));
 end
 
-function F = callPDEFunctionWithReshape(pdefun, t, domain, y, shape)
-    f = pdefun(t, domain, reshapeInput(y, shape));
+function F = pdefunVector(pdefun, t, domain, y, shape)
+    y = reshapeToShape(y, shape);
     
-    F = reshapeOutput(y, f, shape);
+    f = pdefun(t, domain, y);
+    
+    F = reshapeToVector(f, shape);
 end
 
-function out = reshapeInput(y, shape)
-    out = reshape(y, [shape, numel(y) / prod(shape)]);
+function Y = reshapeToShape(y, shape)
+    Y = reshape(y, [shape, numel(y) / prod(shape)]);
 end
 
-function F = reshapeOutput(y, f, shape)
-    F = reshape(f, [prod(shape), numel(y) / prod(shape)]);
+function y = reshapeToVector(Y, shape)
+    y = reshape(Y, prod(shape), []);
 end
