88f08fa8f5c0d93b526be87cdae148b5e20f1a79
Paul Alexander <paulalexander23@live.co.uk>
2019-08-02 12:47:32 +0100

    Streamline function.

--- create.m
+++ create.m
@@ -4,16 +4,9 @@ function create(theta, Re, C, xLength, yLength, tFinal, interface, xN, yN, AbsTo
     if nargin < 9, yN = 64; end
     if nargin < 10, AbsTol = 1e-6; end
 
-    x = setupX(xLength, yLength, xN, yN);
-
-    t = setupT(tFinal, 0.2);
-    
-    y0 = interface(x);
-
     params = [1, theta, Re, C]; % delta, theta, Re, C
     problemDiffDegrees = [1, 0; 0, 1; 2, 0; 0, 2]';
-
-    domain = FDDomain(x, problemDiffDegrees, 4);
+    domain = FDDomain(setupX(xLength, yLength, xN, yN), problemDiffDegrees, 4);
     pdeFunction = @(t, domain, y) fbenney2d(domain, y, params);
 
     odeopt = odeset( ...
@@ -25,7 +18,7 @@ function create(theta, Re, C, xLength, yLength, tFinal, interface, xN, yN, AbsTo
     timeStepper = @(odefun, t, y0) ode15s(odefun, t, y0, odeopt);
 
     tic
-    [y, t] = pdeSolver(pdeFunction, t, domain, y0, timeStepper);
+    [y, t] = pdeSolver(pdeFunction, setupT(tFinal, 0.2), domain, interface(domain.x), timeStepper);
     timeTaken = toc;
 
     saveData(y, params, t, domain.x, timeTaken, tFinal, interface, AbsTol)
