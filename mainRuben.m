f68d3782749d5ecb00f7bd4e9b1fc395095f94ae
Paul Alexander <paulalexander23@live.co.uk>
2019-08-02 11:36:03 +0100

    Update the scripts for ruben.

--- mainRuben.m
+++ mainRuben.m
@@ -1,19 +1,30 @@
-addpath discretisationMethods
-
+% addpath discretisationMethods
+addpath ../discretisation-methods/
 
 x = setupX(32,32,64,64);
 params = [1, 7 * pi / 8, 1, 0.01]; % delta, theta, Re, C
-domain = FDDomain(x, [1, 0; 0, 1; 2, 0; 0, 2]', 4); % Finite Difference Domain
-% domain = PSDomain(x); % Finite Difference Domain
+domain = FDDomain(x, [1, 0; 0, 1; 2, 0; 0, 2]', 4);
+% domain = PSDomain(x);
 pdefun = @(t, domain, y) fwibl1(domain, y, params);
 
-% h = load('wibl1DataForRuben.mat','h'); % Note: h is y and F combine
+vars = load('wibl1DataForRuben.mat','h'); % Note: h is y and F combine
+h = vars.h;
 
-y = h(1:end/2,:);
-F = h(end/2+1:end,:);
-% F = zeros(size(F)); % set F1 to zero instead of using numerics.
+y = (h(1:end/2,:));
+F = (h(end/2+1:end,:));
+% F = 2/3 * ones(size(F)); % set F1 to zero instead of using numerics.
 
-dhdt = fwibl1(domain, h, params);
+%% Smoothing
+fy = fftn(y);
+fy(abs(fy)<1e0) = 0;
+y = real(ifftn(fy));
+
+fF = fftn(F);
+fF(abs(fF)<1e-3) = 0;
+F = real(ifftn(fF));
+
+%%
+dhdt = fwibl1(domain, [y; F], params);
 dydt = dhdt(1:end/2,:);
 dFdt = dhdt(end/2+1:end,:);
 
