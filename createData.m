d5efcf67a2484b65d879c8f46ce59c928c51bd2f
Paul Alexander <paulalexander23@live.co.uk>
2019-08-02 19:48:55 +0100

    Combine the different create files into one.

--- createData.m
+++ createData.m
@@ -1,16 +1,49 @@
 function createData(model, theta, Re, C, xLength, yLength, tFinal, interface, xN, yN, AbsTol, method)
+    addpath discretisationMethods
     if nargin < 9, xN = 64; end
     if nargin < 10, yN = 64; end
     if nargin < 11, AbsTol = 1e-6; end
     if nargin < 12, method = "finite-difference"; end
     
+    params = [1, theta, Re, C]; % delta, theta, Re, C
+    t = setupT(tFinal, 0.2);
+    x = setupX(xLength, yLength, xN, yN);
+    
+    if method == "finite-difference"
+        problemDiffDegrees = [1, 0; 0, 1; 2, 0; 0, 2]';
+        domain = FDDomain(x, problemDiffDegrees, 4);
+    elseif method == "pseudo-spectral"
+        domain = PSDomain(x);
+    end
+    
     if model == "benney"
-        create(theta, Re, C, xLength, yLength, tFinal, interface, xN, yN, AbsTol);
+        y0 = interface(domain.x);
+        odeFunction = @(t, y) fbenney2d(domain, y, params);
+        odeopt = odeset( ...
+            'Jacobian', @(t, y) jbenney2d(domain, y, params), ...
+            ...'Vectorized', 'on', ...
+            'AbsTol', AbsTol ...
+            ... 'BDF','on' ...
+            );
     elseif model == "wibl1"
-        if method == "finite-difference"
-            createWIBL1(theta, Re, C, xLength, yLength, tFinal, interface, xN, yN, AbsTol)
-        elseif method == "pseudo-spectral"
-            createWIBL1PseudoSpectral(theta, Re, C, xLength, yLength, tFinal, interface, xN, yN, AbsTol)
-        end
+        y0 = interface(x);
+        y0 = [y0; 0*y0]; % [y0; F_0]
+        odeFunction = @(t, y) fwibl1(domain, y, params);
+        odeopt = odeset( ...
+            ... 'Vectorized', 'on', ...
+            'AbsTol', AbsTol ...
+            ... 'BDF','on' ...
+            );
+    end
+    
+    timeStepper = @(odefun, t, y0) ode15s(odefun, t, y0, odeopt);
+    tic
+    [y, t] = odeMatrixSolver(odeFunction, t, y0, timeStepper);
+    timeTaken = toc;
+    
+    if model == "benney"
+        saveData(y, params, t, domain.x, timeTaken, tFinal, interface, AbsTol)
+    elseif model == "wibl1"
+        saveData(y, params, t, domain.x, timeTaken, tFinal, interface, AbsTol, "-wibl1")
     end
 end
