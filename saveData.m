96e8b5f3ff0cae16bf1e176daadb7e7c493d2896
Paul Alexander <paulalexander23@live.co.uk>
2020-01-14 10:23:28 +0000

    Remove extra files and make small formatting changes.

--- saveData.m
+++ saveData.m
@@ -1,5 +1,7 @@
 function saveData(y, params, t, x, timeTaken, tFinal, interface, AbsTol, model, prefix)
     if nargin < 10, prefix = ""; end
+    
     filename = makeFilename(prefix, params, x, tFinal, interface, AbsTol, model);
+    
     save(filename, 'y', 'params', 't', 'x', 'timeTaken');
 end
--- struct2str.m
+++ struct2str.m
@@ -1,6 +1,7 @@
 function str = struct2str(struct)
     fields = fieldnames(struct);
     values = struct2cell(struct);
+    
     str = "";
     for i = 1:length(fields)
         str = str + sprintf("-%s-%g", fields{i}, values{i}(end));
