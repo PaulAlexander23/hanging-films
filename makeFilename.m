6ead81c0eb4ef185d510fea5db1b8cffcff41fd2
Paul Alexander <paulalexander23@live.co.uk>
2020-01-15 13:52:51 +0000

    Add timestepper to arguments and stop on nans.

--- makeFilename.m
+++ makeFilename.m
@@ -1,5 +1,5 @@
 function filename = makeFilename(prefix, ivpArguments, timePointsArguments, timeStepperArguments)
     
     filename = prefix + struct2str(ivpArguments) + ...
-        struct2str(timePointsArguments) + struct2str(timeStepperArguments);
+        struct2str(timePointsArguments);% + struct2str(timeStepperArguments);
 end
