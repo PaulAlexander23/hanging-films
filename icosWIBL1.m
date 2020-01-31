81f97621a1e849febdd75cf3a39fcce40cae2e32
Paul Alexander <paulalexander23@live.co.uk>
2020-01-31 13:06:57 +0000

    Update defaults for icosWIBL1

--- icosWIBL1.m
+++ icosWIBL1.m
@@ -1,8 +1,8 @@
 function y0 = icosWIBL1(x, a, b, c, d)
     if nargin < 2, a = 0.25; end
     if nargin < 3, b = 0.25; end
-    if nargin < 4, c = 0.25; end
-    if nargin < 5, d = 0.25; end
+    if nargin < 4, c = 0; end
+    if nargin < 5, d = 0; end
     
     y0 = [1 - a * cos(2*pi/x{1}(end) * x{1}) - ...
         b * cos(2*pi/x{2}(end) * x{2});
