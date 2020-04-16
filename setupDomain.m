f1e37bc0d7f42f0a4cd44e4e5810a85f57816b05
Paul Alexander <paulalexander23@live.co.uk>
2020-04-16 20:00:25 +0100

    Initialise all derivative matrices likely to be useful.

--- setupDomain.m
+++ setupDomain.m
@@ -5,7 +5,7 @@ function domain = setupDomain(args)
     x = setupX(args.xLength, args.yLength, args.xN, args.yN);
 
     if args.method == "finite-difference"
-        problemDiffDegrees = [1, 0; 0, 1; 2, 0; 0, 2]';
+        problemDiffDegrees = [1, 0; 2, 0; 3, 0; 4, 0; 1, 1; 2, 1; 3, 1; 1, 2; 2, 2; 1, 3; 0, 1; 0, 2; 0, 3; 0, 4]';
         accuracy = 4;
         domain = FDDomain(x, problemDiffDegrees, accuracy);
     elseif args.method == "pseudo-spectral"
