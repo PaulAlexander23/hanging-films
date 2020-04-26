04613682feb7d0dc8bafe1c283682897ea35c82c
Paul Alexander <paulalexander23@live.co.uk>
2020-04-26 10:13:51 +0100

    Add tests for the Semi Implicit function evaluation and solving.

--- testSemiImplicit.m
+++ testSemiImplicit.m
@@ -177,3 +177,85 @@ function testSemiImplicitEvolution(testCase)
         F = domain.reshapeToVector(f);
     end
 end
+
+function testSemiImplicit1DbenneyWave(testCase)
+    addpath('discretisationMethods');
+    addpath('~/Repositories/time-stepping-methods');
+
+    xL = 32; yL = 32; xN = 64; yN = 64;
+    x = {linspace(xL/xN,xL,xN),linspace(yL/yN,yL,yN)};
+    domain = FDDomain(x, [1, 0; 2, 0; 0, 1; 0, 2]', 2);
+    params = struct('theta', 7*pi/8, 'Re', 1, 'C', 0.01);
+    
+    fbenney2dExplicitVec = matFuncToVecFunc(@fbenney2dExplicit);
+    explicitOdefun = @(t, y) fbenney2dExplicitVec(domain, y, params);
+
+    t = linspace(0, 40,  1000)';
+
+    load('data/ic-Benney-2D-64.mat', 'h');
+    y0 = domain.reshapeToVector(h);
+
+    timeStepper = @bdf1si;
+
+    options = struct('optimmethod', @(fun, x0) fsolve(fun, x0, ...
+        optimoptions('fsolve', 'Display', 'iter', 'SpecifyObjectiveGradient', true)));
+
+    tic
+    [~, y] = timeStepper(explicitOdefun, @(t, y) implicitOdefun(t, y, domain, params), t, y0, options);
+    timeTaken = toc;
+    y = y';
+    y = permute(y, [1, 3, 2]);
+    y = domain.reshapeToDomain(y);
+
+    size(y)
+    save('temp.mat')
+
+    verifySize(testCase, y, [xN, yN, length(t)]);
+
+    function [F, J] = implicitOdefun(t, y, domain, params)
+        [f, J] = fbenney2dImplicit(domain, domain.reshapeToDomain(y),params);
+        
+        F = domain.reshapeToVector(f);
+    end
+end
+
+function testSemiImplicit2DbenneyWave(testCase)
+    addpath('discretisationMethods');
+    addpath('~/Repositories/time-stepping-methods');
+
+    xL = 32; yL = 32; xN = 64; yN = 64;
+    x = {linspace(xL/xN,xL,xN),linspace(yL/yN,yL,yN)};
+    domain = FDDomain(x, [1, 0; 2, 0; 0, 1; 0, 2]', 2);
+    params = struct('theta', 7*pi/8, 'Re', 1, 'C', 0.01);
+    
+    fbenney2dExplicitVec = matFuncToVecFunc(@fbenney2dExplicit);
+    explicitOdefun = @(t, y) fbenney2dExplicitVec(domain, y, params);
+
+    t = linspace(0, 20,  200)';
+
+    load('data/ic-Benney-3D-64.mat', 'h');
+    %load('tempic.mat', 'h');
+    y0 = domain.reshapeToVector(h);
+
+    timeStepper = @bdf2si;
+
+    options = struct('optimmethod', @(fun, x0) fsolve(fun, x0, ...
+        optimoptions('fsolve', 'Display', 'iter', 'SpecifyObjectiveGradient', true)));
+
+    tic
+    [~, y] = timeStepper(explicitOdefun, @(t, y) implicitOdefun(t, y, domain, params), t, y0, options);
+    timeTaken = toc;
+    y = y';
+    y = permute(y, [1, 3, 2]);
+    y = domain.reshapeToDomain(y);
+
+    save('temp.mat')
+
+    verifySize(testCase, y, [xN, yN, length(t)]);
+
+    function [F, J] = implicitOdefun(t, y, domain, params)
+        [f, J] = fbenney2dImplicit(domain, domain.reshapeToDomain(y),params);
+        
+        F = domain.reshapeToVector(f);
+    end
+end
