f432556de6e4711004a650ac7d7a3faf52e3db3c
Paul Alexander <paulalexander23@live.co.uk>
2020-07-07 17:01:09 +0100

    Use implicit forcing in timestepper test.

--- mainTestTimeStepper.m
+++ mainTestTimeStepper.m
@@ -19,9 +19,9 @@ function mainTestTimeStepper(theta, Re, C, xL, yL, tFinal, xN, yN, timeStepper, 
 
     y0 = domain.reshapeToVector(h0);
 
-    explicitOdefun = @(t, y) fbenney2dExplicit(domain, y, params) ...
+    explicitOdefun = @(t, y) fbenney2dExplicit(domain, y, params);
+    implicitOdefun = @(t, y) fbenney2dImplicit(domain, y, params) ...
           + fbenneyforcing(t, domain, params);
-    implicitOdefun = @(t, y) fbenney2dImplicit(domain, y, params);
     odefun = struct('explicit', explicitOdefun, 'implicit', implicitOdefun);
     odejac = @(t, y) jbenney2dImplicit(domain, y, params);
 
