6bf1edb15c116eda7bf18293f49f8c28590d9198
Paul Alexander <paulalexander23@live.co.uk>
2020-07-28 11:48:56 +0100

    Fix bug in periodicInterp2 and add tests.

--- periodicInterp2.m
+++ periodicInterp2.m
@@ -2,17 +2,20 @@ function Vq = periodicInterp2(X, Y, V, Xq, Yq, method)
     %Assuming equal spacing between points x0, x1 and x2
     if nargin < 6, method = 'linear'; end
 
+    V = [V(:,end), V];
+    V = [V(end,:); V];
+
     if isvector(X)
         X = [X(2)-2*X(1), reshape(X, 1, [])];
         Y = [Y(2)-2*Y(1), reshape(Y, 1, [])];
+
+        Vq = interp2(Y, X, V, Yq, Xq, method);
     else
         X = [X(:,2)-2*X(:,1), X];
         X = [X(end,:); X];
         Y = [Y(2,:)-2*Y(1,:); Y];
         Y = [Y(:,end), Y];
-    end
-    V = [V(:,end), V];
-    V = [V(end,:); V];
 
-    Vq = interp2(X, Y, V, Xq, Yq, method);
+        Vq = interp2(X, Y, V, Xq, Yq, method);
+    end
 end
