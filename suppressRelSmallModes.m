74f315115c1e74149ec58277498336ed24f494be
Paul Alexander <paulalexander23@live.co.uk>
2020-10-08 14:13:37 +0100

    Split out relative mode suppression.

--- suppressRelSmallModes.m
+++ suppressRelSmallModes.m
@@ -1,5 +1,5 @@
 function yhat = suppressSmallModes(yhat, suppression)
     if nargin < 2, suppression = 1e-13; end
 
-    yhat(abs(yhat)<suppression) = 0;
+    yhat(abs(yhat)<suppression * max(max(abs(yhat)))) = 0;
 end
